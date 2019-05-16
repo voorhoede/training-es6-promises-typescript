cf07c51fd030e0d9f59a931b900517e18341560a
Declan Rek <declanrek@gmail.com>
2019-05-16 11:23:47 +0200

    sync

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function getGroceries(list) {
   setMessage('Getting groceries: <br><br>' + list.join(',<br>'));
   return new Promise((resolve, reject) => {
     if (document.getElementById('outOfSpaghetti').checked) {
-      reject('There\'s no of spaghetti!');
+      reject('There\'s no spaghetti!');
     }
     setTimeout(function(){
       resolve(list);
@@ -39,6 +39,9 @@ function getGroceries(list) {
 }
 
 function makeDinner(ingredients) {
+  if (!ingredients.length) {
+    return reject('<strong style="color:red;">I don\'t have any ingredients, I\'m getting pizza</strong>')
+  }
   setMessage('Making dinner');
   return new Promise((resolve, reject) => {
     setTimeout(function(){
